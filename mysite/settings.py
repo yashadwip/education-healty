e267cb465dc32f1cfa804219dc29414a0fa2940c
yashadwip <ydwiprianti@gmail.com>
2025-06-27 20:39:27 +0800

    update settings.py to use env variables

--- mysite/settings.py
+++ mysite/settings.py
@@ -21,11 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b5=g%3j#yoal=$y^#y#aw0_ptg!af^kp&x=%8m)*^-3t#$5^lq'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-default-key')
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
