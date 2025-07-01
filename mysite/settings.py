aed2335bc8c8cc580151ecd5a96c6c2d7e3c1feb
yashadwip <ydwiprianti@gmail.com>
2025-07-01 21:30:27 +0800

    Add .gitignore

--- mysite/settings.py
+++ mysite/settings.py
@@ -21,11 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-default-key')
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['yashadwip18.pythonanywhere.com']
 
 
 # Application definition
@@ -86,14 +88,13 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 
 DATABASES = {
-     'default': {
-         'ENGINE' : 'django.db.backends.sqlite3',
-         'NAME' : BASE_DIR / 'db.sqlite3',
-         'USER' : 'root',
-         'PASSWORD' : '',
-         'HOST' : 'localhost',
-         'PORT' : '3306'
-     }
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'yashadwip18$default',
+        'USER': 'yashadwip18',
+        'PASSWORD': 'yashadwiprianti',
+        'HOST': 'yashadwip18.mysql.pythonanywhere-services.com',
+    }
 }
 
 # Password validation
